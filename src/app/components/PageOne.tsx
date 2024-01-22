3c9a2e2d23a10587c61bbf7378770fb85904d518
Abdullahi254 <abdush504@gmail.com>
2024-01-22 23:56:42 +0300

    created a grid component for videobox card

--- src/app/components/PageOne.tsx
+++ src/app/components/PageOne.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { FaRobot } from "react-icons/fa";
 import { MdOutlineStarPurple500 } from "react-icons/md";
-import dp from "../../../public/dp.jpg";
+import dp6 from "../../../public/ani-6.jpg";
+import dp1 from "../../../public/ani-1.jpg";
+import dp2 from "../../../public/ani-2.jpg";
+import dp3 from "../../../public/ani-3.jpg";
 import Image from "next/image";
+import VideoBox from './VideoBox';
 type Props = {}
 
 const PageOne = (props: Props) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-10 py-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4 px-10 py-4">
             {/* first grid */}
             <div className='flex flex-col space-y-8 px-4'>
                 {/* title heading */}
@@ -25,7 +29,7 @@ const PageOne = (props: Props) => {
                         Start your free trial
                     </button>
                     <div className='flex items-center'>
-                        <FaRobot className='text-blue-600' />
+                        <FaRobot className='text-blue-600 hidden md:inline-block' />
                         <button className="bg-white hover:bg-gray-100 text-blue-600 font-semibold py-2 px-4 rounded-full text-xs md:text-sm">
                             Discover AI assistant
                         </button>
@@ -34,10 +38,10 @@ const PageOne = (props: Props) => {
                 {/* avatars and star ratings */}
                 <div className='flex space-x-4 flex-wrap'>
                     <div className='flex'>
-                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp} alt="Rounded avatar" />
-                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp} alt="Rounded avatar" />
-                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp} alt="Rounded avatar" />
-                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp} alt="Rounded avatar" />
+                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp6} alt="Rounded avatar" />
+                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp1} alt="Rounded avatar" />
+                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp2} alt="Rounded avatar" />
+                        <Image className="w-10 h-10 rounded-full -mx-2" src={dp3} alt="Rounded avatar" />
                     </div>
                     <div className='flex flex-col space-y-2'>
                         <div className='flex space-x-2 items-center'>
@@ -54,10 +58,8 @@ const PageOne = (props: Props) => {
                     </div>
                 </div>
             </div>
-
             {/* second grid */}
-
-
+            <VideoBox/>
         </div>
 
     )
--- src/app/components/VideoBox.tsx
+++ src/app/components/VideoBox.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import dp1 from "../../../public/ani-1.jpg";
+import dp2 from "../../../public/ani-2.jpg";
+import dp3 from "../../../public/ani-3.jpg";
+import dp4 from "../../../public/ani-4.jpg";
+import dp5 from "../../../public/ani-5.jpg";
+import dp6 from "../../../public/ani-6.jpg";
+import { IoMicOutline, IoVideocamOutline } from "react-icons/io5";
+import { CiMonitor, CiFaceSmile, CiSettings } from "react-icons/ci";
+import { FaComment } from "react-icons/fa";
+
 
 type Props = {}
 
 const VideoBox = (props: Props) => {
   return (
-    <div className='grid'>
-
+    <div className="bg-blue-50 p-8 rounded-xl shadow-md">
+      {/* animation avator grid */}
+      <div className='grid grid-cols-3 gap-4 mb-6'>
+        <Image className="w-16 h-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-2xl" src={dp1} alt="Rounded avatar" />
+        <Image className="w-16 h-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-2xl" src={dp2} alt="Rounded avatar" />
+        <Image className="w-16 h-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-2xl" src={dp3} alt="Rounded avatar" />
+        <Image className="w-16 h-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-2xl" src={dp4} alt="Rounded avatar" />
+        <Image className="w-16 h-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-2xl" src={dp5} alt="Rounded avatar" />
+        <Image className="w-16 h-16 sm:w-32 sm:h-32 lg:w-40 lg:h-40 rounded-2xl" src={dp6} alt="Rounded avatar" />
+      </div>
+      {/* icons layout */}
+      <div className='flex flex-wrap space-x-4 w-full justify-center'>
+        <div className='rounded-full bg-white p-2'>
+          <IoMicOutline className='text-blue-500 text-lg' />
+        </div>
+        <div className='rounded-full bg-white p-2'>
+          <IoVideocamOutline className='text-blue-500 text-lg' />
+        </div>
+        <div className='rounded-full bg-white p-2'>
+          <CiMonitor className='text-blue-500 text-lg' />
+        </div>
+        <div className='rounded-full bg-white p-2'>
+          <CiFaceSmile className='text-blue-500 text-lg' />
+        </div>
+        <div className='rounded-full bg-white p-2'>
+          <FaComment className='text-blue-500 text-lg' />
+        </div>
+        <div className='rounded-full bg-white p-2'>
+          <CiSettings className='text-blue-500 text-lg' />
+        </div>
+      </div>
     </div>
   )
 }
